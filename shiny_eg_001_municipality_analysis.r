d0ac06ea9e074f4c6c3352b03fcddfa91161d675
f-caro <fa.caro.aguilera@gmail.com>
2022-02-17 22:26:19 -0300

    using read.csv(), changed to fluidPage and unique() municipality list

--- shiny_eg_001_municipality_analysis.r
+++ shiny_eg_001_municipality_analysis.r
@@ -1,59 +1,50 @@
 library(shiny)
+library(arrow, warn.conflicts = FALSE)  # Add CSV speedups to script
+library(dplyr, warn.conflicts = FALSE)
+
+#DPLYR ver of read.csv() # 
+csv_muni_old <- read.csv("./TA_Subsidios_beneficios.csv", 
+                         sep=";" , 
+                         #nrows=10,
+                         encoding="latin1" ,
+)
+
+select_input_startup <- unique(csv_muni_old$organismo_nombre)
 
 # Define UI for app that draws a histogram ----
-ui <- bootstrapPage(
+ui <- fluidPage(
   
   # App title ----
-  titlePanel("Hello Shiny!"),
+  titlePanel("Municipality Analysis"),
   
-  navbarMenu(title="Hello navbarMenu"),
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
-    sidebarPanel(
+  #not working properly#  navbarMenu(title="Hello navbarMenu"),
+
+      # Input: select for the number of organismo_nombre ----
+      selectInput(inputId = "select_input_organismo_nombre",
+                  label = "List of Organizations",
+                  choices = select_input_startup ,
+                  #selected = select_input_startup[0]
+                  ),
       
-      # Input: Slider for the number of bins ----
+
       sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      ,
-      
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "runner",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-      
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      
+                  label="slider Input box",
+                  min=1,
+                  max=50,
+                  value=25
+                        ),
       # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-      ,
-      plotOutput(outputId = "distPlot2")
-      ,
+      plotOutput(outputId = "distPlot"),
+
+      
       dataTableOutput(outputId = "dataTbl1")
-    )
-  )
 )
+
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
   # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
     x    <- faithful$waiting
@@ -62,24 +53,23 @@ server <- function(input, output) {
     hist(x, breaks = bins, col = "#75AADB", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times")
-    
   })
-  
-  output$distPlot2 <- renderPlot({
-    
-    x    <- faithful$waiting
-    runner <- seq(min(x), max(x), length.out = input$runner + 1)
-    
-    hist(x, breaks = runner, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
-    
-  })
+
   
   output$dataTbl1 <- renderDataTable({
+    #DPLYR ver of read.csv() # 
+    filtered_muni <- csv_muni_old %>% 
+                    filter(  
+                      organismo_nombre == input$select_input_organismo_nombre)
+
     
-    csv_muni <- read.csv("./TA_Subsidios_beneficios.csv", sep=";" , nrows=10)
     
+    #having issue with Arrow library and Latin characters...  
+    #arrow lib, for optimal CSV loadin, should be done at start of script
+    #data <- read_delim_arrow("./TA_Subsidios_beneficios.csv", delim=";" ) 
+    #Don't know howto pass UTF8 parameters to read_delim_arrow()
+    #csv_muni_ref <- data %>% head(10) %>% collect()
+    #csv_muni <- data.frame(csv_muni_ref)
   })
 }
 
