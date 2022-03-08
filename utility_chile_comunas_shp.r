1bea4d23ac0193574d4c3f4d03a719339e97f57c
f-caro <fa.caro.aguilera@gmail.com>
2022-03-08 13:53:51 -0300

    loaded shp, had to st_transform('+proj=longlat +datum=WGS84') -- now working in Leaflet(), without tmap manipulation

--- utility_chile_comunas_shp.r
+++ utility_chile_comunas_shp.r
@@ -1,14 +1,19 @@
 ptm <- proc.time()  #Starting PROCESS time counter
+print(getwd())
+setwd(getwd())
+#setwd("/home/i7mintu/Documents/PROJECTS/R-ShinyProj/shiny_eg_001_municipality_analysis")
 
 # Example Source: https://stackoverflow.com/questions/31873151/how-rotate-map-in-r?noredirect=1&lq=1
 ####################################################################################################
 
-library(sf)
 library(ggplot2)
-library(dplyr)
 library(tmap)
 library(tmaptools)
+library(dplyr)
 library(leaflet)
+library(sf)
+library(shiny)
+library(stringr)
 
 
 # Rotate an sf geom around a center point. If no center is
@@ -29,16 +34,27 @@ st_ellide_rotate = function(x, degrees, center_coords=NULL){
 
 df_muni <- readRDS(file = "df_muni_leaf.Rds")
 
-chile_comunas_shp <- st_read("chile_comunas_maps/comunas.shp")  # size 64MB
-# if( !file.exists("chile_comunas_shp.rds") ) {
-#   chile_comunas_shp <- st_read("chile_comunas_maps/comunas.shp")  # size 64MB
-#   saveRDS(chile_comunas_shp, file = "chile_comunas_shp.Rds")
-#   #loading shp file :::#   #user  system elapsed #   #3.537   0.126   4.016
-# } else {
-#   chile_comunas_shp <- readRDS(file = "chile_comunas_shp.Rds")  # size 32MB
-#   #loading from Rds file --- shows the same amount of time taken
-#   #user  system elapsed                          #   #3.622   0.108   4.130
-# }
+
+if( !file.exists("chile_comunas_shp_joined.Rds") ) {
+  chile_comunas_shp <- st_read("chile_comunas_maps/comunas.shp")  # size 64MB
+  df_muni_comuna_summary <- readRDS(file="df_muni_comuna_summary.Rds")
+  
+  chile_comunas_shp_joined <- chile_comunas_shp %>% 
+    left_join(df_muni_comuna_summary, 
+              by = c( "Comuna" = "COMUNA" )
+              )
+  
+  saveRDS(chile_comunas_shp_joined, file = "chile_comunas_shp_joined.Rds")
+
+} else {
+  chile_comunas_shp_joined <- readRDS(file = "chile_comunas_shp_joined.Rds")
+}
+
+#chile_comunas_shp_joined <- readRDS(file = "chile_comunas_shp_joined.Rds")
+
+chile_comunas_shp_joined <- chile_comunas_shp_joined %>% sf::st_transform('+proj=longlat +datum=WGS84')
+# proj=longlat converts shp file coords to be compatible with Leaflet input structure,
+
 
 proc.time() - ptm
 print(paste0("loading shp/RDS file \n"))  # not worth it, probably better to use DB
@@ -50,6 +66,7 @@ print(paste0("loading shp/RDS file \n"))  # not worth it, probably better to use
 
 ui <- bootstrapPage(
   actionButton(inputId = "buttonInsertTmap", label = "InsertTmap Layer" ),
+  actionButton(inputId = "buttonInsertRegionNuble", label = "InsertTmap Region BioBio Layer" ),
   leafletOutput(outputId = "leafletMap" , width = "100%", height = "800px"), # height= 100% doesn't pass through
 )
 
@@ -62,15 +79,16 @@ ui <- bootstrapPage(
   #tm_map <- tm_shape( chile_comunas_shp %>% filter(Region == "Región del Bío-Bío") ) +
   #          tm_polygons()   # Region BioBio takes ::: 2.705 seconds to load
   
-  tm_map <- tm_shape( chile_comunas_shp %>% filter(Region == "Región del Bío-Bío") ) +
-            tm_polygons( "st_area_sh" ,
-                         n= 5, 
-                         style="sd",
-                         border.col = "white", 
-                         lwd = 0.5, lty = "dashed" , 
-                         legend.show = FALSE, 
-                         palette= "RdYlGn"
-                         )   
+  # tm_map <- tm_shape( chile_comunas_shp_joined ) + #%>% filter( REGION == "Región del Bío-Bío" ) ) +
+  #           tm_polygons( "TOTAL_MONTO_PER_COMUNA" ,
+  #                        alpha= 0.55,
+  #                        n= 100, 
+  #                        style="quantile",
+  #                        border.col = "white", 
+  #                        lwd = 0.5, lty = "dashed" , 
+  #                        legend.show = FALSE, 
+  #                        palette= c("green", "red")
+  #                        )
   # Region BioBio takes ::: 2.705 seconds to load
 
 server <- function(input, output) {
@@ -96,47 +114,70 @@ server <- function(input, output) {
   # proc.time() - ptm
   # print(paste0("finished plotting to PDF file \n"))
 
-  
-  observeEvent(input$buttonInsertTmap, {
-    leafletProxy("leafletMap", data = df_muni ) %>%
-      setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
-      addTiles() %>%
-      addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
-      addProviderTiles(providers$CartoDB.Positron)  %>%
-      # addMarkers(
-      #   ~LON, ~LAT,
-      #   #icon = if( is.na( ~TOTAL_MONTO ) ) { leafletIcon } else { NULL } ,
-      #   icon = iconQuestionMarkPng,
-      #   popup = ~MSG_SUMMARY,
-      #   label = ~MSG_SUMMARY ,
-      #   layerId = ~ORG_NOMBRE ,
-      #   data = df_muni %>% filter( is.na(TOTAL_MONTO)  ),
-      # ) %>%
-      addCircleMarkers(
-        ~LON, ~LAT,
-        popup = ~MSG_SUMMARY,
-        label = ~MSG_SUMMARY ,
-        layerId = ~ORG_NOMBRE ,
-        radius = 20, #radiusColSrcName(1),      #radius = ~CIRCLE_RADIUS_MONTO_TOTAL,
-        stroke = FALSE, fillOpacity = 0.5,
-        data = df_muni %>% filter( !is.na(TOTAL_MONTO) ),
-      ) 
+  observeEvent(input$buttonInsertRegionNuble, {
+    filtered_data <- chile_comunas_shp_joined %>%  filter( REGION == "Región Del Biobío") 
+    leafletProxy("leafletMap", data = filtered_data ) %>%
+      # setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
+      # addTiles() %>%
+      # addProviderTiles(providers$CartoDB.Positron)  %>%
+      addPolygons( color = "#444444", weight = 1, smoothFactor = 0.5,
+                   opacity = 1.0, fillOpacity = 0.5,
+                   fillColor = ~colorQuantile("YlOrRd", TOTAL_MONTO_PER_COMUNA)(TOTAL_MONTO_PER_COMUNA),
+                   highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                       bringToFront = TRUE),
+                   label = ~str_c(Comuna, " - CLP $ " , format( TOTAL_MONTO_PER_COMUNA, 
+                                                          scientific = FALSE, 
+                                                          big.mark = ","
+                                                          )
+                                  ),
+                   )
   }, ignoreNULL = TRUE)
   
-  output$leafletMap <- renderLeaflet({
-    tmap_leaflet(tm_map, in.shiny=TRUE, options = pathOptions(pane = "layer2")) %>%
-      setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
-      addTiles() %>%
-      addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
-      addProviderTiles(providers$CartoDB.Positron) %>%
-      addMapPane("layerTop", zIndex=420) %>% addMapPane("layer2",zIndex=410)
+  observeEvent(input$buttonInsertTmap, {
+    print(paste0("this was clicked,  buttonInsertTmap"))
+    
+    # leafletProxy("leafletMap", data = df_muni ) %>%
+    #   setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
+    #   addTiles() %>%
+    #   addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
+    #   addProviderTiles(providers$CartoDB.Positron)  %>%
+    #   # addMarkers(
+    #   #   ~LON, ~LAT,
+    #   #   #icon = if( is.na( ~TOTAL_MONTO ) ) { leafletIcon } else { NULL } ,
+    #   #   icon = iconQuestionMarkPng,
+    #   #   popup = ~MSG_SUMMARY,
+    #   #   label = ~MSG_SUMMARY ,
+    #   #   layerId = ~ORG_NOMBRE ,
+    #   #   data = df_muni %>% filter( is.na(TOTAL_MONTO)  ),
+    #   # ) %>%
+    #   addCircleMarkers(
+    #     ~LON, ~LAT,
+    #     popup = ~MSG_SUMMARY,
+    #     label = ~MSG_SUMMARY ,
+    #     layerId = ~ORG_NOMBRE ,
+    #     radius = 20, #radiusColSrcName(1),      #radius = ~CIRCLE_RADIUS_MONTO_TOTAL,
+    #     stroke = FALSE, fillOpacity = 0.5,
+    #     data = df_muni %>% filter( !is.na(TOTAL_MONTO) ),
+    #   ) 
+  }, ignoreNULL = TRUE)
+  
+  # output$leafletMap <- renderLeaflet({
+  #   tmap_leaflet(tm_map, in.shiny=TRUE, options = pathOptions(pane = "layer2")) %>%
+  #     setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
+  #     addTiles() %>%
+  #     addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
+  #     addProviderTiles(providers$CartoDB.Positron) %>%
+  #     addMapPane("layerTop", zIndex=420) %>% addMapPane("layer2",zIndex=410)
+  # 
+  # })
 
-  })
-    leafletProxy("leafletMap", data = df_muni  ) %>%
+  output$leafletMap <- renderLeaflet({
+    leaflet(data = df_muni ) %>%
       setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
       addTiles() %>%
       addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
       addProviderTiles(providers$CartoDB.Positron)  %>%
+      addMapPane("layerTop", zIndex=420) %>% addMapPane("layer2",zIndex=410) %>%
       # addMarkers(
       #   ~LON, ~LAT,
       #   #icon = if( is.na( ~TOTAL_MONTO ) ) { leafletIcon } else { NULL } ,
@@ -151,12 +192,41 @@ server <- function(input, output) {
         popup = ~MSG_SUMMARY,
         label = ~MSG_SUMMARY ,
         layerId = ~ORG_NOMBRE ,
-        radius = 20, #radiusColSrcName(1),      #radius = ~CIRCLE_RADIUS_MONTO_TOTAL,
+        radius = 5, #radius = radiusColSrcName(1),      #radius = ~CIRCLE_RADIUS_MONTO_TOTAL,
         stroke = FALSE, fillOpacity = 0.5,
         data = df_muni %>% filter( !is.na(TOTAL_MONTO) ),
         options = pathOptions(pane = "layerTop"),
-      ) 
+      )
     
+    #### tried to remove addMarkers() and addCircleMarkers in order to just run function -->   changeRadiusVectorSrc(1)  --- did NOT work
+  })
+    
+  # output$leafletMap <- renderLeaflet({
+  #   leaflet( data = df_muni  ) %>%   ##########   WATCH out when COPY/pasting from leafletProxy() function ---  the ID is autoidentified
+  #     setView(lat = -36.82699, lng = -73.04977, zoom = 10) %>% 
+  #     addTiles() %>%
+  #     addMiniMap( position = "bottomleft", zoomAnimation = TRUE , zoomLevelOffset = -4) %>%
+  #     addProviderTiles(providers$CartoDB.Positron)  %>%
+  #     # addMarkers(
+  #     #   ~LON, ~LAT,
+  #     #   #icon = if( is.na( ~TOTAL_MONTO ) ) { leafletIcon } else { NULL } ,
+  #     #   icon = iconQuestionMarkPng,
+  #     #   popup = ~MSG_SUMMARY,
+  #     #   label = ~MSG_SUMMARY ,
+  #     #   layerId = ~ORG_NOMBRE ,
+  #     #   data = df_muni %>% filter( is.na(TOTAL_MONTO)  ),
+  #     # ) %>%
+  #     addCircleMarkers(
+  #       ~LON, ~LAT,
+  #       popup = ~MSG_SUMMARY,
+  #       label = ~MSG_SUMMARY ,
+  #       layerId = ~ORG_NOMBRE ,
+  #       radius = 5, #radiusColSrcName(1),      #radius = ~CIRCLE_RADIUS_MONTO_TOTAL,
+  #       stroke = FALSE, fillOpacity = 0.5,
+  #       data = df_muni %>% filter( !is.na(TOTAL_MONTO) ),
+  #       options = pathOptions(pane = "layerTop"),
+  #     ) 
+  # })
 }
  
 
