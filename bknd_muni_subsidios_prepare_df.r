2de4495cac6bd357dd1f60f8d2dec82d9bc142e8
f-caro <fa.caro.aguilera@gmail.com>
2022-03-07 22:20:41 -0300

    needed specific function to create DF of Comunas summary

--- bknd_muni_subsidios_prepare_df.r
+++ bknd_muni_subsidios_prepare_df.r
@@ -87,7 +87,7 @@ filterOrgDataByValidCoords <- function( df_org )
   return(df_org_upd_filtered)
 }
 
-innerJoinSubsidiesWithOrgData <- function ( df_subsidies, df_org , removeNAs = false  )
+innerJoinSubsidiesWithOrgData <- function ( df_subsidies, df_org , removeNAs = false , ...  )
 {
   #STEP 2.3: Combine Gov Subsidies with Gov Org df's
   df_muni_subsidios_upd <- df_subsidies %>% 
@@ -96,7 +96,8 @@ innerJoinSubsidiesWithOrgData <- function ( df_subsidies, df_org , removeNAs = f
     summarise( ORG_NOMBRE = unique(organismo_nombre) ,
                COUNT_N = n(), 
                TOTAL_MONTO =  sum( monto_global , na.rm= removeNAs ),
-               LAT_LON = unique(lat_lon)
+               LAT_LON = unique(lat_lon),
+               ...
     )
   return(df_muni_subsidios_upd)
 }
@@ -122,7 +123,38 @@ leafletDfPrepwork <- function ( df_muni ){
 }
 
 
-main_bknd_muni_subsidios_prepare_df <- function( df ) {
+prepMuniComunaSummary <- function(govSubCsvLocation , orgCsvLocation )
+{
+  
+  df_muni_fixed <- fixTipoUnidadMonetariaCol( loadFullSubsidies( govSubCsvLocation) ) #"TA_Subsidios_beneficios.csv" ) )
+  df_muni_org <- loadOrgDataUpdatedWithCoords( orgCsvLocation ) #"organismos360_updated.csv")
+  
+  df_muni_sum_per_org  <- df_muni_fixed %>% 
+    group_by( organismo_nombre ) %>% 
+    summarise(ORG_COD = unique(organismo_codigo) , 
+              ORG_NOMBRE = unique(organismo_nombre) , 
+              COUNT_ENTRIES_PER_ORG = n() , 
+              TOTAL_MONTO_PER_ORG = sum(monto_global, na.rm=TRUE) 
+    )
+  
+  df_muni_sum_per_org_joined_muni_orgs <- df_muni_sum_per_org %>% 
+    left_join(df_muni_org, by = c( "ORG_COD" = "Codigo_org" )) 
+
+  df_muni_comunas_summary <- df_muni_sum_per_org_joined_muni_orgs %>%  
+    group_by( Municipalidad ) %>% 
+    summarise( COMUNA = unique( str_to_title(Municipalidad) ),
+               REGION = unique( str_to_title(Region) ), 
+               COUNT_ORGS_PER_COMUNA = n(), 
+               COUNT_NUM_ENTRIES_PER_COMUNA = sum(COUNT_ENTRIES_PER_ORG),
+               TOTAL_MONTO_PER_COMUNA =  sum( TOTAL_MONTO_PER_ORG , na.rm= TRUE ),
+    )
+  
+  return( df_muni_comunas_summary )
+}
+
+
+
+main_leaflet_visuals <- function( df ) {
   
   #STEP 3.0: Leaflet preview output
   leafMap <- leaflet(data = df ) %>%
@@ -133,7 +165,7 @@ main_bknd_muni_subsidios_prepare_df <- function( df ) {
 
 }
 
-main_recalc_df_muni_df_summary_df_muni_leaf_RDSfiles <- function ()
+main_recalc_all_df_from_csv <- function ()
 {
   ptm <- proc.time()
   print(paste0("started function::: main_recalc_df_muni_df_summary_df_muni_leaf_RDSfiles"))
@@ -146,19 +178,24 @@ main_recalc_df_muni_df_summary_df_muni_leaf_RDSfiles <- function ()
   saveRDS(df_muni_summary, file = "df_muni_summary.Rds")  
   
   df_muni_summary_dismissNAs <- processSubsidiesXOrganismos( "TA_Subsidios_beneficios.csv" , 
-                                                          "organismos360_updated.csv" , 
-                                                          dismissNAs=TRUE  )
+                                                             "organismos360_updated.csv" , 
+                                                             dismissNAs=TRUE  )
   saveRDS(df_muni_summary_dismissNAs, file = "df_muni_summary_dismissNAs.Rds")
     
   df_muni_leaf <- leafletDfPrepwork( df_muni_summary )
   saveRDS(df_muni_leaf, file = "df_muni_leaf.Rds")
   
-  #measure Time taken to process this script
   
+  df_muni_comuna_summary <- prepMuniComunaSummary("TA_Subsidios_beneficios.csv" , 
+                                                        "organismos360_updated.csv" )
+  saveRDS(df_muni_comuna_summary, file = "df_muni_comuna_summary.Rds")
+  
+  #measure Time taken to process this script
   print(paste0("ended function::: main_recalc_df_muni_df_summary_df_muni_leaf_RDSfiles"))
   proc.time() - ptm
 }
-  
+
+
 
 # runs only when script is run by itself & in Interactive Mode
 #if (getOption('run.main', default=TRUE)) {
